6434b0917d8aacda5dcc73acd8fa73d41efd4d35
Lian <nail3105@gmail.com>
2024-02-06 18:51:34 -0500

    added go debugger and some custom mappings

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,8 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = { theme = 'onedark' }
 M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
 
 return M
